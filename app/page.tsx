1df68b0a92cabecc56e72a26555addf274f2ced5
BM-Hayes <bmhayes@proton.me>
2026-08-29 09:43:54 -0400

    Pass Mapbox token

--- app/page.tsx
+++ app/page.tsx
@@ -5,5 +5,6 @@ export const revalidate = 300;
 
 export default async function HomePage() {
   const listings = await getPublishedListings();
-  return <AtlasApp listings={listings} />;
+  const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN ?? "";
+  return <AtlasApp listings={listings} mapboxToken={mapboxToken} />;
 }
