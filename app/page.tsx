529ca73b242d83d5920cee701404de66125c540f
BM-Hayes <bmhayes@proton.me>
2026-08-29 09:57:12 -0400

    Read MAPBOX_TOKEN on server

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ export const revalidate = 300;
 
 export default async function HomePage() {
   const listings = await getPublishedListings();
-  const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN ?? "";
+  const mapboxToken =
+    process.env.MAPBOX_TOKEN ||
+    process.env.NEXT_PUBLIC_MAPBOX_TOKEN ||
+    "";
   return <AtlasApp listings={listings} mapboxToken={mapboxToken} />;
 }
